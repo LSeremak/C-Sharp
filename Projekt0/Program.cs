69a69ff90372cb1e53c2764f2fff07893fafeb2b
Łukasz Seremak <33153470+LSeremak@users.noreply.github.com>
2019-03-19 14:19:45 +0100

    Test

--- Projekt0/Program.cs
+++ Projekt0/Program.cs
@@ -10,8 +10,40 @@ namespace Projekt0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Your name:");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + name);
+
+            Console.WriteLine("Dziś jest:");
+            Console.WriteLine(DateTime.Now.DayOfWeek);
+
+            //int first = 20;
+            // int second = 30;
+            // int third = first + second;
+            // Console.WriteLine(third / 2);
+
+
+
+            Console.WriteLine("Jak długo dziś spaleś?");
+
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            if (hoursOfSleep > 8)
+            {
+                Console.WriteLine("Jesteś wyspany");
+            }
+            else
+            {
+                Console.WriteLine("Oj niedobrze, powinieneś spać dłużej");
+            }
+
+
+
+
             Console.ReadKey();
         }
     }
+
+        
 }
