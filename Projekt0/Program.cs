7777ac8eb8b6b8b3280a774c91ee8f14b383f86d
Łukasz Seremak <33153470+LSeremak@users.noreply.github.com>
2019-04-08 08:01:53 +0200

    Add

--- Projekt0/Program.cs
+++ Projekt0/Program.cs
@@ -10,33 +10,99 @@ namespace Projekt0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Your name:");
-            string name = Console.ReadLine();
 
-            Console.WriteLine("Hello " + name);
-
-            Console.WriteLine("Dziś jest:");
-            Console.WriteLine(DateTime.Now.DayOfWeek);
-
-            //int first = 20;
-            // int second = 30;
-            // int third = first + second;
-            // Console.WriteLine(third / 2);
+            //string min = "test";
+            //string min2 = "test 2";
 
 
+            //console.writeline(min);
+            //console.writeline(min2);
 
-            Console.WriteLine("Jak długo dziś spaleś?");
+            //min2 = "test 3";
 
-            int hoursOfSleep = int.Parse(Console.ReadLine());
 
-            if (hoursOfSleep > 8)
-            {
-                Console.WriteLine("Jesteś wyspany");
-            }
-            else
-            {
-                Console.WriteLine("Oj niedobrze, powinieneś spać dłużej");
-            }
+            //console.writeline(min2);
+
+
+
+            string firstName;  // zmienna przechowyje imie
+            string lastName; // zminenna przechowuje nazwisko
+            string thanks = "Dziekuję!";
+
+
+
+            Console.WriteLine("Hej ty");
+            Console.Write("Wprowadź imię :");
+            /* Console.Write wuswietla wpisany ciag znakow w zminnnej  w jednym wierszu */
+
+            firstName = Console.ReadLine();
+
+            Console.Write("Wprowadź nazwisko :");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine(thanks);
+
+
+
+            Console.WriteLine(
+                $"Twoje imie i nazwisko to {firstName} {lastName}"
+                );
+
+
+
+            double liczba1, liczba2, wynik;
+           
+
+            Console.WriteLine("Podaj pierwszą liczbę:");
+            liczba1 = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Podaj drugą liczbę:");
+            liczba2 = Double.Parse(Console.ReadLine());
+
+            wynik = liczba1 + liczba2;
+
+            Console.WriteLine("\a");
+
+            Console.WriteLine("Wynik:" + wynik);
+
+            Console.WriteLine(@"Poczatek
+                    /\
+                   /  \
+                  /    \
+Koniec"
+);
+
+            string lenght = "Ile to jest znaków";
+
+            Console.WriteLine(lenght);
+
+            Console.WriteLine(lenght.Length);
+
+
+            string text, uppercase;
+
+            Console.WriteLine("Wprowadź tekst:");
+            text = Console.ReadLine();
+
+            Console.WriteLine("A teraz zaminimy test na duże litery");
+
+            uppercase = text.ToUpper();
+
+            Console.WriteLine(uppercase);
+
+
+           
+            
+
+            
+
+            
+
+
+
+
+
+
 
 
 
